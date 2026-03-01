c11375d446bf87b44ab6fe6c958921816f6b804d
Ariel <xarielx@udel.edu>
2026-03-01 07:44:23 -0500

    changed extenstion url

--- policy-lens/background.js
+++ policy-lens/background.js
@@ -1,6 +1,4 @@
-chrome.action.onClicked.addListener((tab) => {
-  // URL of full application
-  const fullAppUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXCgBwHSBwkJvgoBhyohjO8%3D";
-
+chrome.action.onClicked.addListener(() => {
+  const fullAppUrl = "http://localhost:5173/"; // change if needed
   chrome.tabs.create({ url: fullAppUrl });
 });
